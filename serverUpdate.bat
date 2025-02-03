075633a1537378b80d17709f62b23e02aadfb2e2
OrionScripts <59019352+OrionScripts@users.noreply.github.com>
2025-02-03 13:29:03 -0500

    Create serverUpdate.bat
    This file should be placed with other server files and can be updated as the batch file that triggers server start. It ensures the server software is always up to date with what's available from Astrolabe Interactive.

---
+++ serverUpdate.bat
@@ -0,0 +1,2 @@
+"I:\steamcmd\steamcmd.exe" +force_install_dir "PATH\TO\SERVER\FILES" +login OWNING_ACCOUNT +app_update 1660080 validate +quit /WAIT
+start "" "PATH\TO\SERVER\FILES\triggerLoadServer.bat"
